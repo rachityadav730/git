6f526ae0dc3b9b495cfe541a0a0ac1dbf8f1e71e
rachityadav730 <73478053+rachityadav730@users.noreply.github.com>
2023-06-08 23:12:44 +0530

    new updated branch check git vslues

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-console.log(" remove this line run js in vs code")
+console.log(" remove this line run js in vs code check git values")
 
 var a = "b"
-var b = "chnage value of b changes from c to this"
+var b = "chnage value of b changes from c to this check git values"
 
 var c = a + b
 
-console.log("index.js two changes done merge these file to main branch or other branch",c)
+console.log("index.js two changes done merge these file to main branch or other branch check git values",c)
