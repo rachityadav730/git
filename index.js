d68db47b91c266cd4ca54c86f0c90c5bef8efdb9
rachityadav730 <73478053+rachityadav730@users.noreply.github.com>
2023-06-08 22:51:34 +0530

    all changes in updated branches

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-console.log("run js in vs code")
+console.log(" remove this line run js in vs code")
 
 var a = "b"
-var b = "c"
+var b = "chnage value of b changes from c to this"
 
 var c = a + b
 
-console.log("index.js",c)
+console.log("index.js two changes done merge these file to main branch or other branch",c)
