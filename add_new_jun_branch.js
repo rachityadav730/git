1302bc6d60f6b7a3b4f6989cad20bbf003594cc1
rachityadav730 <73478053+rachityadav730@users.noreply.github.com>
2023-07-13 00:33:56 +0530

    add this changes to  main file

--- add_new_jun_branch.js
+++ add_new_jun_branch.js
@@ -1,2 +1,10 @@
 console.log("file added new branch")
-console.log(" add console from pullgitcode folder")
+console.log("please change this line")
+
+var a = 786 
+var b  = 987
+
+var c = a +b
+console.log("summation of a and b",c)
+
+conaole.log("aad also this line")
