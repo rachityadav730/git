27638c29e941cc27eba5ae3c5fa21f6fea44bb73
rachityadav730 <73478053+rachityadav730@users.noreply.github.com>
2023-07-16 11:42:39 +0530

    done changes in cloning bracnh

--- add_file_in_first_chnges_branch.js
+++ add_file_in_first_chnges_branch.js
@@ -3,3 +3,9 @@ console.log("chnage this line to privious line")
 console.log("add done some chnagesin cloning file")
 
 console.log("Adfdfasadfadfa")
+
+var b = "radhe radhe in cloneing bracnh"
+var a  = "varable a in clomning branch"
+
+var c = a +b
+console.log("get value of c in clonmeing value",c)
