4bd95600f10bd14167053dcd6523e36e1c6e7ec7
rachityadav730 <73478053+rachityadav730@users.noreply.github.com>
2023-07-16 11:39:01 +0530

    add all changes main file and push main file

--- add_file_in_first_chnges_branch.js
+++ add_file_in_first_chnges_branch.js
@@ -3,3 +3,12 @@ console.log("chnage this line to privious line")
 console.log("add done some chnagesin cloning file")
 
 console.log("Adfdfasadfadfa")
+
+console.log("add some chnages in main files")
+
+var a = 7788
+var b = 23424
+
+var c = a +b
+
+console.log("get values of c",c)
